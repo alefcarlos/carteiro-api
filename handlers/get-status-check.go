713e269ea388ed379e70fb02968699ee3cbf50b4
= <alef.carlos@gmail.com>
2018-02-05 19:24:29 -0200

    feat: migrando para httprouter

--- handlers/get-status-check.go
+++ handlers/get-status-check.go
@@ -1,12 +1,17 @@
 package handlers
 
 import (
-	"github.com/alefcarlos/carteiro-api/utils"
+	"fmt"
+	"net/http"
 
-	"github.com/gin-gonic/gin"
+	"github.com/julienschmidt/httprouter"
 )
 
+// func GetStatus(c *gin.Context) {
+// 	utils.SendSuccess(c, "tudo na paz, irmão!")
+// }
+
 //GetStatus retorna a lista de rastreios que têm novas informações
-func GetStatus(c *gin.Context) {
-	utils.SendSuccess(c, "tudo na paz, irmão!")
+func GetStatus(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprint(w, "Welcome!\n")
 }
--- main.go
+++ main.go
@@ -1,36 +1,43 @@
 package main
 
 import (
+	"log"
+	"net/http"
+
 	"github.com/alefcarlos/carteiro-api/handlers"
-	"github.com/gin-gonic/gin"
+	"github.com/julienschmidt/httprouter"
 )
 
 //GetMainEngine obtém as configurações do server
-func GetMainEngine() *gin.Engine {
-	r := gin.Default()
+// func GetMainEngine() *gin.Engine {
+// 	r := gin.Default()
 
-	r.GET("/status", handlers.GetStatus)
+// 	r.GET("/status", handlers.GetStatus)
 
-	_trackingGroup := r.Group("/tracking")
-	{
-		_trackingGroup.GET("/", handlers.GetAvailableTrackings)
-		_trackingGroup.PUT("/:id", handlers.PutTrackingInfo)
-		_trackingGroup.PUT("/:id/seen", handlers.PutTrackingSeen)
-	}
+// 	_trackingGroup := r.Group("/tracking")
+// 	{
+// 		_trackingGroup.GET("/", handlers.GetAvailableTrackings)
+// 		_trackingGroup.PUT("/:id", handlers.PutTrackingInfo)
+// 		_trackingGroup.PUT("/:id/seen", handlers.PutTrackingSeen)
+// 	}
 
-	_subscribeGroup := r.Group("/subscribe")
-	{
-		_subscribeGroup.POST("/", handlers.PostNewSubscribe)
-	}
+// 	_subscribeGroup := r.Group("/subscribe")
+// 	{
+// 		_subscribeGroup.POST("/", handlers.PostNewSubscribe)
+// 	}
 
-	_notify := r.Group("/notify")
-	{
-		_notify.POST("/all", handlers.NotImplementedYet)
-	}
+// 	_notify := r.Group("/notify")
+// 	{
+// 		_notify.POST("/all", handlers.NotImplementedYet)
+// 	}
 
-	return r
-}
+// 	return r
+// }
 
 func main() {
-	GetMainEngine().Run(":3000")
+	router := httprouter.New()
+
+	router.GET("/status", handlers.GetStatus)
+
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
